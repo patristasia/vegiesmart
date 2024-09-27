3830650d6d0460d3c0df5df973de1e0cad3e01d0
alfianlensundev <alfianlensundev@gmail.com>
2024-09-27 21:06:16 +0800

    navbar

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,16 +1,35 @@
+import {useState, useEffect} from "react"
 import { Navbar, Container, Nav, Button } from "react-bootstrap";
 
 import {navLinks} from '../data/index';
 import {NavLink} from "react-router-dom";
+import React from "react";
+
 
 const NavbarComponent = () => {
+
+  const [changeColor, setChangeColor] =useState(false);
+
+  const changeBackgroundColor = () => {
+    if (window.scrollY > 10) {
+      setChangeColor(true);
+    } else {
+      setChangeColor(false)
+    };
+  };
+
+  useEffect (() => {
+    changeBackgroundColor();
+
+    window.addEventListener("scroll", changeBackgroundColor)
+  })
+
   return (
     <div>
-       <Navbar expand="lg" fixed="top">
+       <Navbar expand="lg" className={changeColor ? "color-active" : ""}>
           <Container>
             <div><img src="/logo.png" alt="" className="custom-img"/></div>
-            <Navbar.Brand href="#home" className="fs-4 fw-normal">Vegies Mart
-            </Navbar.Brand>
+            <Navbar.Brand href="#home" className="fs-4 fw-normal">Vegies Mart</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="mx-auto text-center">
