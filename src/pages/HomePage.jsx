db70c3e2f7acee12088a832ab3e0ddfeb9317759
patristasia <patrisiaanastasia17@gmail.com>
2024-09-30 18:02:06 +0800

    fix

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,7 +7,19 @@ const HomePage = () => {
     <div className="homepage">
       <header className="w-100 min-vh-100">
         <Container>
-          
+          <Row className="header-box">
+          <Col>
+            <h1>Your Health <br />
+            <span>is in Ours!</span> <br /> Make your choice now.
+            </h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet voluptatibus qui in, praesentium quia itaque.</p>
+            <button>View Products</button>
+            <button>View Promos</button>
+          </Col>
+          <Col>
+          <img src={HeroImage} alt="hero-img" />
+          </Col>
+          </Row>
         </Container>
       </header>
       <div className="vegetable w-100 min-vh-100"></div>
