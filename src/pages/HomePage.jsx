c158e1e167e9f2a585f8787ff7052fcf63923054
patristasia <patrisiaanastasia17@gmail.com>
2024-09-30 11:17:17 +0800

    fix

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,15 @@
+import {Container, Row, Col} from "react-bootstrap";
+import HeroImage from '../assets/img/veghero.png';
+
+
 const HomePage = () => {
   return (
     <div className="homepage">
-      <header className="w-100 min-vh-100"></header>
+      <header className="w-100 min-vh-100">
+        <Container>
+          
+        </Container>
+      </header>
       <div className="vegetable w-100 min-vh-100"></div>
     </div>
   )
