9e29b83c811bb5fdd5782d802d86ebcf5f354253
alfianlensundev <alfianlensundev@gmail.com>
2024-10-02 16:51:33 +0800

    homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,18 +5,21 @@ import HeroImage from '../assets/img/veghero.png';
 const HomePage = () => {
   return (
     <div className="homepage">
-      <header className="w-100 min-vh-100">
+      <header className="w-100 min-vh-100 d-flex align-items-center pt-lg-5">
         <Container>
-          <Row className="header-box">
-          <Col>
-            <h1>Your Health <br />
-            <span>is in Ours!</span> <br /> Make your choice now.
+          <Row className="header-box d-flex align-items-center">
+          <Col lg="6">
+            <h1 className="mb-4">Your Health <br />
+            is in Ours! <br />
+            <span>Make your choice now.</span>
             </h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet voluptatibus qui in, praesentium quia itaque.</p>
-            <button>View Products</button>
-            <button>View Promos</button>
+            <p className="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet voluptatibus qui in, praesentium quia itaque.</p>
+            <div className="button-container">
+            <button className="btn_viewProducts">View Products</button>
+            <button className="btn_viewPromos">View Promos</button>
+            </div>
           </Col>
-          <Col>
+          <Col lg="6"className="pt-lg-0 pt-4">
           <img src={HeroImage} alt="hero-img" />
           </Col>
           </Row>
